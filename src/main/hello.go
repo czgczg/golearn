5aef739a797cc342c8afafa568458cb43c4af278
W9001005 <W9001005@adc.com>
2019-07-19 11:30:29 +0800

    docs: update readme.md

--- src/main/hello.go
+++ src/main/hello.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"test"
 )
 
 func main() {
-	fmt.Println("start app")
+	hello := test.SayHello()
+	count := 18
+	fmt.Println(hello)
 
-	fmt.Print(test.SayHello())
+	fmt.Println(reflect.TypeOf(hello))
+	fmt.Println(&hello)
+	fmt.Println(reflect.TypeOf(count))
 
+	const Ln2 = 0.693147180559945309417232121458176568075500134360255254120680009693147180559945309417232121458176568075500134360255254120680009;
+	var a = float32(Ln2)
+
+	fmt.Println(Ln2)
+	fmt.Println(a)
 }
